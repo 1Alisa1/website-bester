4b8610b0f9552fd1b4f6324fe9fe99b78a3ec7a1
1Alisa1 <Alisa.dragun@mail.ru>
2023-02-18 23:38:05 +0300

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import { Layout } from './components/layout/layout';
 import { HomePage } from './pages/homePage/homePage';
 import { NotFoundPage } from './pages/notFoundPage/notFoundPage';
 import { ServicesPage } from './pages/servicesPage/servicesPage';
@@ -7,9 +8,11 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Layout />} >
+          <Route index element={<HomePage />} />
         <Route path="/services" element={<ServicesPage />} />
         <Route path="*" element={<NotFoundPage />} />
+        </Route>
       </Routes>
     </>
   );
