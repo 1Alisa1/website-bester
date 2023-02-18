6bba1671b7dad358c64a8538361f4dcac75b2e10
1Alisa1 <Alisa.dragun@mail.ru>
2023-02-18 13:47:13 +0300

    add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import { HomePage } from './pages/homePage/homePage';
+import { NotFoundPage } from './pages/notFoundPage/notFoundPage';
+import { ServicesPage } from './pages/servicesPage/servicesPage';
+
 function App() {
-  return <div className="App">fgghhg</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
