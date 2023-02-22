a5b9c3623a185ee754feb7d066097298042e1470
1Alisa1 <Alisa.dragun@mail.ru>
2023-02-22 19:25:52 +0300

    add questions section

---
+++ src/components/question/question.tsx
@@ -0,0 +1,37 @@
+import styles from './question.module.scss';
+import { useState } from 'react';
+
+interface QuestionProps {
+  question: string;
+  answer: string;
+  isAlwaysOpen: boolean;
+}
+
+const Question: React.FC<QuestionProps> = ({ question, answer, isAlwaysOpen }) => {
+  const [showAnswer, setShowAnswer] = useState(isAlwaysOpen);
+
+  const handleClick = () => {
+    if (isAlwaysOpen) {
+      setShowAnswer(true);
+    } else {
+      setShowAnswer(!showAnswer);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.question}>
+        <div>
+          <p>{question}</p>
+          <button className={showAnswer ? `${styles.button} ${styles.open}`: styles.button} onClick={handleClick}></button>
+        </div>
+        <div className={styles.border}></div>
+      </div>
+      <div className={showAnswer ? `${styles.answer} ${styles.show}` : styles.answer }>
+         <p>{answer}</p> 
+      </div>
+    </div>
+  );
+};
+
+export default Question;
---
+++ src/components/questionsSection/questionsSection.tsx
@@ -0,0 +1,34 @@
+import Question from '../question/question';
+import styles from './questionsSection.module.scss';
+
+const QuestionsSection = () => {
+  return (
+    <section>
+      <h2>Ваши вопросы</h2>
+      <div className={styles.questionsContainer}>
+        <Question
+          question="С какой суммы можно начать товарный бизнес?"
+          answer="Тебе достаточно 100$ в кармане, чтобы начать товарный бизнес. У нас есть поставщики, которые готовы работать по системе Дропшиппинга, что позволяет значительно снизить расходы на запуск"
+          isAlwaysOpen={true}
+        />
+        <Question
+          question="С чего начинать?"
+          answer="Достаточно оставить заявку на сайте, и наш менеджер свяжется с тобой. Наши менеджеры проконсультируют по тарифам и подберут подходящий вариант"
+          isAlwaysOpen={false}
+        />
+        <Question
+          question="Можно поднять тариф?"
+          answer="Да, конечно! В случае если ты захочешь поднять свой тариф, то достаточно будет доплатить разницу между тарифами и всё"
+          isAlwaysOpen={false}
+        />
+        <Question
+          question="Если я из другой страны, мне подойдут ваши услуги?"
+          answer="Да, наши тарифы подходят для Беларуси, России и Украины"
+          isAlwaysOpen={false}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default QuestionsSection;
