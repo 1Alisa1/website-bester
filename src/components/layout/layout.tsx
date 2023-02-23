d4022a82f7e5047ffb24e25d7fbb699e169e00f3
1Alisa1 <Alisa.dragun@mail.ru>
2023-02-23 18:16:56 +0300

    fix styles according to design layout changes

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -45,34 +45,36 @@ const Layout = () => {
         <Outlet />
       </main>
       <footer>
-        <div className={styles.footerLogo}>
-          <Link to="/">
-            <img src={logo} alt="BESTEP"></img>
-          </Link>
-        </div>
-        <div className={styles.footerInfo}>
-          <div className={styles.firstBlock}>
-            <div className={styles.individual}>
-              Индивидуальный предприниматель <br /> Андреев Е.Л. УНП 691537014
+        <div className={styles.content}>
+          <div className={styles.footerLogo}>
+            <Link to="/">
+              <img src={logo} alt="BESTEP"></img>
+            </Link>
+          </div>
+          <div className={styles.footerInfo}>
+            <div className={styles.firstBlock}>
+              <div className={styles.individual}>
+                Индивидуальный предприниматель <br /> Андреев Е.Л. УНП 691537014
+              </div>
+              <div className={styles.location}>
+                Республика Беларусь, Минская <br /> область, Минский район
+              </div>
             </div>
-            <div className={styles.location}>
-              Республика Беларусь, Минская <br /> область, Минский район
+            <div className={styles.secondBlock}>
+              <div className={styles.accountNumber}>
+                BY25UNBS30131130500010000933
+              </div>
+              <div className={styles.bank}>в ЗАО “БСБ-Банк”, БИК: UNBSBY2X</div>
             </div>
           </div>
-          <div className={styles.secondBlock}>
-            <div className={styles.accountNumber}>
-              BY25UNBS30131130500010000933
+          <div className={styles.footerContacts}>
+            <div className={styles.mode}>
+              Режим работы: <br /> пн-вс: 09.00 — 19.00
             </div>
-            <div className={styles.bank}>в ЗАО “БСБ-Банк”, БИК: UNBSBY2X</div>
+            <div className={styles.email}>bestep.by@gmail.com</div>
+            <div className={styles.phoneNumber}>+ 375 (25) 686 54 60</div>
+            <IconsGroup iconClass={styles.icon60} />
           </div>
-        </div>
-        <div className={styles.footerContacts}>
-          <div className={styles.mode}>
-            Режим работы: <br /> пн-вс: 09.00 — 19.00
-          </div>
-          <div className={styles.email}>bestep.by@gmail.com</div>
-          <div className={styles.phoneNumber}>+ 375 (25) 686 54 60</div>
-          <IconsGroup iconClass={styles.icon60} />
         </div>
       </footer>
     </>
