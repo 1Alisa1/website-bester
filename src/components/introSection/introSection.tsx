fca0f4caae029119360bf446d44f7e7e5aae5bfb
1Alisa1 <Alisa.dragun@mail.ru>
2023-02-24 12:48:11 +0300

    add intro section

---
+++ src/components/introSection/introSection.tsx
@@ -0,0 +1,24 @@
+import styles from './introSection.module.scss';
+import img from "../../img/first-img.png";
+
+const IntroSection: React.FC = () => {
+  return (
+    <section className={styles.intro}>
+      <div className={styles.info}>
+        <h1 className={styles.title}>
+          <span>BESTEP</span> — бизнес для каждого
+        </h1>
+        <p className={styles.subtitle}>Мы продаём и создаём готовые интернет-магазины под ключ</p>
+        <div className={styles.imageSmall}>
+          <img src={img} alt="Мы продаём и создаём готовые интернет-магазины под ключ" />
+        </div>
+        <button className={styles.contact}>Связаться</button>
+      </div>
+      <div className={styles.image}>
+        <img src={img} alt="Мы продаём и создаём готовые интернет-магазины под ключ" />
+      </div>
+    </section>
+  );
+};
+
+export default IntroSection;
--- src/pages/homePage/homePage.tsx
+++ src/pages/homePage/homePage.tsx
@@ -1,8 +1,10 @@
+import IntroSection from '../../components/introSection/introSection';
 import QuestionsSection from '../../components/questionsSection/questionsSection';
 
 const HomePage: React.FC = () => {
   return (
     <>
+      <IntroSection />
       <QuestionsSection />
     </>
   );
